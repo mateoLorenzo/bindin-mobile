b1c49cc8ee3000d098b36320f529eddb80119c8b
mateolorenzo <mateolorenzo.dev@gmail.com>
2025-05-28 19:47:40 -0300

    Feat: Added project fonts - OpenSauceOne and Poppins

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,19 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    OpenSauceOneRegular: require("../assets/fonts/OpenSauceOne-Regular.ttf"),
+    OpenSauceOneMedium: require("../assets/fonts/OpenSauceOne-Medium.ttf"),
+    OpenSauceOneSemiBold: require("../assets/fonts/OpenSauceOne-SemiBold.ttf"),
+    OpenSauceOneBold: require("../assets/fonts/OpenSauceOne-Bold.ttf"),
+    PoppinsRegular: require("../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) return null;
   return (
     <Stack>
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
---
+++ src/components/AppText/index.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, TextProps } from "react-native";
+
+type TextVariants = "title" | "subtitle" | "button" | "label" | "body";
+
+interface Props extends TextProps {
+  variant?: TextVariants;
+}
+
+export function AppText({ variant = "body", style, ...rest }: Props) {
+  return <Text style={[styles[variant], style]} {...rest} />;
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "OpenSauceOneBold",
+    fontSize: 24,
+    color: "#fff",
+  },
+  subtitle: {
+    fontFamily: "OpenSauceOneSemiBold",
+    fontSize: 20,
+    color: "#fff",
+  },
+  button: {
+    fontFamily: "OpenSauceOneSemiBold",
+    fontSize: 16,
+    color: "#fff",
+  },
+  label: {
+    fontFamily: "OpenSauceOneRegular",
+    fontSize: 14,
+    color: "#ADADAD",
+  },
+  body: {
+    fontFamily: "OpenSauceOneRegular",
+    fontSize: 12,
+    color: "#fff",
+  },
+});
